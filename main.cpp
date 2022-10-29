7e373a1b8d594de7e156825bba99d9a6cbbd2436
Matthew Tibbitts <mitibbitts@uri.edu>
2022-10-28 22:31:48 -0400

    Update main.cpp
    reset to all includes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include "aClass.h"
 #include "Units.h"
 #include "UserInterface.h"
+#include "aClass.cpp"
+#include "Units.cpp"
+#include "UserInterface.cpp"
 
 int main() {
 	UserInterface interfaceInstance = UserInterface();
