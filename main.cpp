1dd5258695346bead27e37ce3630d692eed31450
JacobLy0271 <98777321+JacobLy0271@users.noreply.github.com>
2022-10-28 22:26:35 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,11 +2,8 @@
 #include "aClass.h"
 #include "Units.h"
 #include "UserInterface.h"
-#include "UserInterface.cpp"
-#include "aClass.cpp"
-#include "Units.cpp"
 
-int main(){ 
+int main() {
 	UserInterface interfaceInstance = UserInterface();
 	interfaceInstance.primaryInterface();
 	return 0;
